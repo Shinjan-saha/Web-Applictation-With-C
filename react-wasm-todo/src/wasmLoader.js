314581e418bd7dbc5fce532a3bc2ec4c6ea09075
Shinjan-saha <shinjansaha02@gmail.com>
2024-07-16 20:33:45 +0530

    Added code

--- react-wasm-todo/src/wasmLoader.js
+++
@@ -1,7 +0,0 @@
-export async function loadWasmModule() {
-    const response = await fetch('/todo_queue.wasm');
-    const buffer = await response.arrayBuffer();
-    const { instance } = await WebAssembly.instantiate(buffer);
-    return instance.exports;
-  }
-  
